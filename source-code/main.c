f752458f761b898b4f99e2ed2e4ffa47c6434aad
unknown <jlindblom@.sparkfun.local>
2010-09-13 16:11:01 -0600

    Lots of quick, minimal fixes.  Lots more to come! -------------------------------------------- - Reorganized main.c and Serial-7-Seg.h. Only function prototypes, defines, and includes are in the .h file.
    - Optimized a few of the global variable types and made volatile where necessary.
    
    - Got rid of a silly 1 argument switch statement.

--- source-code/main.c
+++ source-code/main.c
@@ -4,11 +4,27 @@
     Spark Fun Electronics
 	by: Jim Lindblom
 	
-	License: Creative Commons Attribution Share-Alike 3.0
-	http://creativecommons.org/licenses/by-sa/3.0
+	License: 
 */
 
 #include "Serial-7-Seg.h"
+
+// Global variables
+//=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+volatile char data0=0;	// Digit 0 data
+volatile char data1=0;	// Digit 1 data
+volatile char data2=0;	// Digit 2 data
+volatile char data3=0;	// Digit 3 data
+volatile char receiveCount = 0;	// Will count from 0 to 3
+volatile char brightDelay = 0;	// Read from EEPROM on startup
+volatile char DPStatus = 0x00;	// Decimal point status, each bit represents one DP
+volatile char uartMode = 0;
+volatile char spiMode = 1;
+volatile char prevData0=0;	// Previous Digit 0 data
+volatile char prevData1=0;	// Previous Digit 1 data
+volatile char prevData2=0;	// Previous Digit 2 data
+volatile char prevData3=0;	// Previous Digit 3 data
+//=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 
 //=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 
@@ -153,7 +169,7 @@ int main(void)
 		display(0, 6);
 		delay_us(DIGIT_ON_TIME);
 		clearDisplay();
-		delay_us(brightDelay*BRIGHT_FACTOR);
+		delay_us((uint16_t) brightDelay*BRIGHT_FACTOR);
 	}
 }
 
@@ -387,12 +403,10 @@ void clearDisplay(void)
 
 void check_Special(void)
 {
-	switch (receiveCount)
+	if (receiveCount == 2)
 	{
-		case 2:
-			prevData2 = data2;
-			prevData3 = data3;
-			break;	
+		prevData2 = data2;
+		prevData3 = data3;
 	}
 	
 	// If sent special character v
@@ -531,4 +545,112 @@ void resetDisplay(void)
 	data1 = 'x';
 	data2 = 'x';
 	data3 = 'x';
+}
+
+void ioinit(void)
+{
+	char junk;
+
+	sei();	// Enable interrupts
+
+	// ************************************************** //
+	//             Data directions:
+	//               1 - output
+	//               0 - input
+	// Port B: (XTAL2)(APOS-A)(SCK)(MISO)(MOSI)(CSN)(NC)(NC)
+	//            i      o     i    o     i     i   -   -
+	DDRB = 0b01010000;
+	// Port C: (RESET)(NC)(DP)(COL-A)(DIG4)(DIG3)(DIG2)(DIG1)
+	//            i    -   o     o     o     o     o     o
+	DDRC = 0b00111111;
+	// Port D: (G)(F)(E)(D)(C)(B)(A)(RX)
+	//          o  o  o  o  o  o  o  i
+	DDRD = 0b11111110;
+	// ************************************************** //
+
+	//Init Timer0 for delay_us
+	//Set Prescaler to clk/8 : 1click = 1us. CS01=1 
+	TCCR0B = (1<<CS01); 
+
+	// intialize USART Baud rate: 9600
+	// Will default to 9600 if bad value in EEPROM
+	// enable rx and rx interrupt
+    UBRR0H = 51 >> 8;
+    UBRR0L = 51;
+    UCSR0B = (1<<RXCIE0)|(1<<RXEN0);
+
+	// Blink CSN for Test
+	DDRB = 0b00000100;
+	sbi(PORTB, 2);
+	delay_ms(100);
+	cbi(PORTB, 2);
+
+	/* Set MISO output, PB6 output, all others input */
+	DDRB = 0b01010000;
+	/* Enable SPI */
+	SPCR = (1<<SPIE) | (1<<SPE);
+	/* Clear SPDR */
+	junk = SPDR;
+}
+
+// Long delays
+void delay_ms(uint16_t x)
+{
+	for (; x > 0 ; x--)
+	{
+		delay_us(250);
+		delay_us(250);
+		delay_us(250);
+		delay_us(250);
+	}
+}
+
+// For short delays
+void delay_us(uint16_t x)
+{
+	if (x != 0)
+	{
+		while(x > 256)
+		{
+			TIFR0 = (1<<TOV0); //Clear any interrupt flags on Timer2
+			TCNT0 = 0; //256 - 125 = 131 : Preload timer 2 for x clicks. Should be 1us per click
+			while( (TIFR0 & (1<<TOV0)) == 0);
+			
+			x -= 256;
+		}
+	
+		TIFR0 = (1<<TOV0); //Clear any interrupt flags on Timer2
+		TCNT0 = 256 - x; //256 - 125 = 131 : Preload timer 2 for x clicks. Should be 1us per click
+		while( (TIFR0 & (1<<TOV0)) == 0);
+	}
+}
+
+// Write toWrite to addr in EEPROM
+void writeEEPROM(char toWrite, uint8_t addr)
+{
+	// Write toWrite value to EEPROM address addr
+	/* Wait for completion of previous write */
+	while(EECR & (1<<EEPE))
+		;
+	/* Set up address and Data Registers */
+	EEAR = addr;
+	EEDR = toWrite;	// Write data1 into EEPROM
+	/* Write logical one to EEMPE */
+	EECR |= (1<<EEMPE);
+	/* Start eeprom write by setting EEPE */
+	EECR |= (1<<EEPE);
+}
+
+// Read EEPROM address addr and return value in EEDR
+char readEEPROM(char addr)
+{
+	/* Wait for completion of previous write */
+	while(EECR & (1<<EEPE))
+		;
+	/* Set up address register */
+	EEAR = addr;
+	/* Start eeprom read by writing EERE */
+	EECR |= (1<<EERE);
+
+	return EEDR;
 }
